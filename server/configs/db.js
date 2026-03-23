3bb2c52bec96f220ebafdb19aa73f6c0f3aa0d1f
Rrr7362 <rohitsrajput7987@gmail.com>
2026-03-23 15:21:45 +0530

    initial commit

---
+++ server/configs/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(`${process.env.MONGODB_URI}/car-rental`);
+
+    console.log(`MongoDB Connected ✅: ${conn.connection.host}`);
+  } catch (error) {
+    console.log("DB Error:", error.message);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
